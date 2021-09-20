23757d804d56f5584e15d9248851f0389088fbd1
Taylor Ono <Taylor.R.Ono@gmail.com>
2021-09-19 22:27:39 -0700

    Added pending spec line markers and abilitiy to enable/disable

--- src/main/java/com/github/idea/ginkgo/util/GinkgoUtil.java
+++ src/main/java/com/github/idea/ginkgo/util/GinkgoUtil.java
@@ -1,11 +1,30 @@
 package com.github.idea.ginkgo.util;
 
-import com.github.idea.ginkgo.GinkgoFunctionType;
+import com.github.idea.ginkgo.GinkgoPendingSpecType;
+import com.github.idea.ginkgo.GinkgoSpecType;
 
 import java.util.Arrays;
 
 public class GinkgoUtil {
     public static boolean isGinkgoFunction(String name) {
-        return Arrays.stream(GinkgoFunctionType.class.getEnumConstants()).anyMatch(e -> e.specType().equals(name));
+        return Arrays.stream(GinkgoSpecType.class.getEnumConstants()).anyMatch(e -> e.specType().equals(name));
+    }
+
+    public static boolean isGinkgoPendingFunction(String name) {
+        return Arrays.stream(GinkgoPendingSpecType.class.getEnumConstants()).anyMatch(e -> e.specType().equals(name));
+    }
+
+    public static GinkgoPendingSpecType getGinkgoPendingSpecType(String name) {
+        return Arrays.stream(GinkgoPendingSpecType.class.getEnumConstants())
+                .filter(e -> e.specType().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new EnumConstantNotPresentException(GinkgoPendingSpecType.class, name));
+    }
+
+    public static GinkgoSpecType getGinkgoSpecType(String name) {
+        return Arrays.stream(GinkgoSpecType.class.getEnumConstants())
+                .filter(e -> e.specType().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new EnumConstantNotPresentException(GinkgoPendingSpecType.class, name));
     }
 }
