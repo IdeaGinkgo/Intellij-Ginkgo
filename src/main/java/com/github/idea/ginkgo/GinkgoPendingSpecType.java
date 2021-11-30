91061cda1ce081cf43e366157acad49e3b12db23
Taylor Ono <Taylor.R.Ono@gmail.com>
2021-11-30 00:08:44 -0800

    Added table extension support
    [#31]

--- src/main/java/com/github/idea/ginkgo/GinkgoPendingSpecType.java
+++ src/main/java/com/github/idea/ginkgo/GinkgoPendingSpecType.java
@@ -4,14 +4,18 @@ import com.github.idea.ginkgo.util.GinkgoUtil;
 
 public enum GinkgoPendingSpecType {
     PDESCRIBE("PDescribe"),
+    PDESCRIBE_TABLE("PDescribeTable"),
     PCONTEXT("PContext"),
     PWHEN("PWhen"),
     PIT("PIt"),
+    PENTRY("PEntry"),
     PSPECIFY("PSpecify"),
     XDESCRIBE("XDescribe"),
+    XDESCRIBE_TABLE("XDescribeTable"),
     XCONTEXT("XContext"),
     XWHEN("XWhen"),
     XIT("XIt"),
+    XENTRY("XEntry"),
     XSPECIFY("XSpecify");
 
     private final String specType;
