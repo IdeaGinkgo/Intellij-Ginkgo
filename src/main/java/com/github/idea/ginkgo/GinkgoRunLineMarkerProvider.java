2929c12728eeb941a94f8b33823aff885c1a6096
Taylor Ono <Taylor.R.Ono@gmail.com>
2021-12-02 23:18:04 -0800

    Disable run feature for table tests with dynamic names
    [#31]

--- src/main/java/com/github/idea/ginkgo/GinkgoRunLineMarkerProvider.java
+++ src/main/java/com/github/idea/ginkgo/GinkgoRunLineMarkerProvider.java
@@ -5,6 +5,7 @@ import com.goide.GoIcons;
 import com.goide.GoTypes;
 import com.goide.execution.testing.GoTestFinder;
 import com.goide.psi.GoCallExpr;
+import com.goide.psi.GoReferenceExpression;
 import com.goide.util.GoUtil;
 import com.intellij.execution.lineMarker.ExecutorAction;
 import com.intellij.execution.lineMarker.RunLineMarkerContributor;
@@ -19,9 +20,13 @@ import com.intellij.util.Function;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
+
 
 public class GinkgoRunLineMarkerProvider extends RunLineMarkerContributor {
     private static final Function<PsiElement, String> TOOLTIP_PROVIDER = element -> "Ginkgo Test";
+    private static final Function<PsiElement, String> TOOLTIP_WARNING = element -> "Unable to focus dynamic test names";
+    private static final Icon disabledTestIcon = GoIcons.Helper.createIconWithShift(AllIcons.RunConfigurations.TestIgnored, AllIcons.Nodes.RunnableMark);;
 
     @Override
     @Nullable
@@ -33,14 +38,28 @@ public class GinkgoRunLineMarkerProvider extends RunLineMarkerContributor {
 
         if (e.getNode().getElementType() == GoTypes.IDENTIFIER && e.getParent().getParent() instanceof GoCallExpr) {
             GoCallExpr parent = (GoCallExpr) e.getParent().getParent();
-            if (GinkgoUtil.isGinkgoFunction(parent.getExpression().getText()) && parent.getArgumentList().getExpressionList().size() >= 2) {
+            String name = parent.getExpression().getText();
+
+            if (GinkgoUtil.isTableEntity(name) && parent.getArgumentList().getExpressionList().size() >= 2) {
+                if (parent.getArgumentList().getExpressionList().get(0) instanceof GoReferenceExpression) {
+                    return new Info(AllIcons.RunConfigurations.TestState.Yellow2, TOOLTIP_WARNING, ActionManager.getInstance().getAction("GinkgoDisableSpec"));
+                }
+            }
+
+            if (GinkgoUtil.isTablePendingEntity(name) && parent.getArgumentList().getExpressionList().size() >= 2) {
+                if (parent.getArgumentList().getExpressionList().get(0) instanceof GoReferenceExpression) {
+                    return new Info(disabledTestIcon, TOOLTIP_PROVIDER, ActionManager.getInstance().getAction("GinkgoEnableSpec"));
+                }
+            }
+
+            if (GinkgoUtil.isGinkgoFunction(name) && parent.getArgumentList().getExpressionList().size() >= 2) {
                 AnAction[] runActions = ExecutorAction.getActions(0);
                 runActions = ArrayUtil.append(runActions, ActionManager.getInstance().getAction("GinkgoDisableSpec"));
                 return new Info(AllIcons.RunConfigurations.TestState.Run, TOOLTIP_PROVIDER, runActions);
             }
 
             if (GinkgoUtil.isGinkgoPendingFunction(parent.getExpression().getText())) {
-                return new Info(GoIcons.Helper.createIconWithShift(AllIcons.RunConfigurations.TestIgnored, AllIcons.Nodes.RunnableMark), TOOLTIP_PROVIDER, ActionManager.getInstance().getAction("GinkgoEnableSpec"));
+                return new Info(disabledTestIcon, TOOLTIP_PROVIDER, ActionManager.getInstance().getAction("GinkgoEnableSpec"));
             }
         }
 
