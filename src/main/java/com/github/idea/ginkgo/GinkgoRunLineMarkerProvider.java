d00110cd70f7f5141245817e1470542a26461e7d
Taylor Ono <Taylor.R.Ono@gmail.com>
2022-10-14 09:47:12 -0700

    Streamline RunLineMarker Logic

--- src/main/java/com/github/idea/ginkgo/GinkgoRunLineMarkerProvider.java
+++ src/main/java/com/github/idea/ginkgo/GinkgoRunLineMarkerProvider.java
@@ -27,8 +27,10 @@ import java.util.List;
 public class GinkgoRunLineMarkerProvider extends RunLineMarkerContributor {
     private static final Function<PsiElement, String> TOOLTIP_PROVIDER = element -> "Ginkgo Test";
     private static final Function<PsiElement, String> TOOLTIP_WARNING = element -> "Unable to focus dynamic test names";
-    private static final Icon disabledTestIcon = GoIcons.Helper.createIconWithShift(AllIcons.RunConfigurations.TestIgnored, AllIcons.Nodes.RunnableMark);
-
+    private static final Icon DISABLED_TEST_ICON = GoIcons.Helper.createIconWithShift(AllIcons.RunConfigurations.TestIgnored, AllIcons.Nodes.RunnableMark);
+    private static final Icon DISABLE_SPEC_ICON = AllIcons.RunConfigurations.TestState.Yellow2;
+    private static final AnAction ENABLE_SPEC_ACTION = ActionManager.getInstance().getAction("GinkgoEnableSpec");
+    private static final AnAction DISABLE_SPEC_ACTION = ActionManager.getInstance().getAction("GinkgoDisableSpec");
     @Override
     @Nullable
     public Info getInfo(@NotNull PsiElement e) {
@@ -40,44 +42,49 @@ public class GinkgoRunLineMarkerProvider extends RunLineMarkerContributor {
         if (e.getNode().getElementType() == GoTypes.IDENTIFIER && e.getParent().getParent() instanceof GoCallExpr) {
             GoCallExpr parent = (GoCallExpr) e.getParent().getParent();
             List<GoExpression> args = parent.getArgumentList().getExpressionList();
-            String name = parent.getExpression().getText();
+            String ginkgoSpec = parent.getExpression().getText();
 
-            if (GinkgoUtil.isTableEntity(name) && args.size() >= 2) {
-                if (args.get(0) instanceof GoReferenceExpression) {
-                    return new Info(AllIcons.RunConfigurations.TestState.Yellow2, TOOLTIP_WARNING, ActionManager.getInstance().getAction("GinkgoDisableSpec"));
-                }
+            if (GinkgoUtil.isTablePendingEntity(ginkgoSpec) && args.size() >= 2 && args.get(0) instanceof GoReferenceExpression) {
+                return new Info(DISABLED_TEST_ICON, TOOLTIP_PROVIDER, ENABLE_SPEC_ACTION);
             }
 
-            if (GinkgoUtil.isTablePendingEntity(name) && args.size() >= 2) {
-                if (args.get(0) instanceof GoReferenceExpression) {
-                    return new Info(disabledTestIcon, TOOLTIP_PROVIDER, ActionManager.getInstance().getAction("GinkgoEnableSpec"));
-                }
+            if (GinkgoUtil.isTableEntity(ginkgoSpec) && args.size() >= 2 && args.get(0) instanceof GoReferenceExpression) {
+                return new Info(DISABLE_SPEC_ICON, TOOLTIP_WARNING, DISABLE_SPEC_ACTION);
             }
 
-            if (GinkgoUtil.isGinkgoFunction(name) && args.size() >= 2) {
-                Icon icon = AllIcons.RunConfigurations.TestState.Run;
-                if (name.equalsIgnoreCase(GinkgoSpecType.IT.specType())) {
-                    List<String> specNames = GinkgoUtil.getSpecNames(e, false);
-                    String specName = specNames.remove(specNames.size() - 1);
-                    String specContext = String.join(" ", specNames);
-                    String testUrl = "gotest://" + GinkgoRunConfigurationProducer.GINKGO + "#" + specContext + "/" + specName;
-                    TestStateStorage.Record record = TestStateStorage.getInstance(e.getProject()).getState(testUrl);
-                    if (record != null) {
-                        icon = getTestStateIcon(record, false);
-                    }
-                }
-
-                AnAction[] runActions = ExecutorAction.getActions(0);
-                runActions = ArrayUtil.append(runActions, ActionManager.getInstance().getAction("GinkgoDisableSpec"));
-                return new Info(icon, TOOLTIP_PROVIDER, runActions);
+            if (GinkgoUtil.isGinkgoPendingFunction(ginkgoSpec)) {
+                return new Info(DISABLED_TEST_ICON, TOOLTIP_PROVIDER, ENABLE_SPEC_ACTION);
             }
 
-            if (GinkgoUtil.isGinkgoPendingFunction(name)) {
-                return new Info(disabledTestIcon, TOOLTIP_PROVIDER, ActionManager.getInstance().getAction("GinkgoEnableSpec"));
+            if (GinkgoUtil.isGinkgoFunction(ginkgoSpec) && args.size() >= 2) {
+                return new Info(getTestIcon(e, ginkgoSpec), TOOLTIP_PROVIDER, getRunActions());
             }
         }
 
         return null;
+    }
+
+    @NotNull
+    private static AnAction[] getRunActions() {
+        //Default executor actions [Run, Debug, DlvRecordAndDebug, Coverage, Profiler, Android Profiler]
+        AnAction[] defaultRunActions = ExecutorAction.getActions(0);
+        return ArrayUtil.append(defaultRunActions, DISABLE_SPEC_ACTION);
+    }
+
+    @NotNull
+    private static Icon getTestIcon(@NotNull PsiElement e, String ginkgoSpec) {
+        Icon icon = AllIcons.RunConfigurations.TestState.Run;
+        if (ginkgoSpec.equalsIgnoreCase(GinkgoSpecType.IT.specType())) {
+            List<String> specNames = GinkgoUtil.getSpecNames(e, false);
+            String specName = specNames.remove(specNames.size() - 1);
+            String specContext = String.join(" ", specNames);
+            String testUrl = "gotest://" + GinkgoRunConfigurationProducer.GINKGO + "#" + specContext + "/" + specName;
+            TestStateStorage.Record record = TestStateStorage.getInstance(e.getProject()).getState(testUrl);
+            if (record != null) {
+                icon = getTestStateIcon(record, false);
+            }
+        }
+        return icon;
     }
 
     public boolean isGinkgoTestFile(PsiFile file) {
